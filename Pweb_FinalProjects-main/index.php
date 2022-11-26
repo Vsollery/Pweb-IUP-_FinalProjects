e79827aec63b207d6d1c41bc8ee6c2ec041db045
Erlangga28 <Erlanggawahyu21@gmail.com>
2022-11-26 17:15:34 +0700

    add edit_admin profile

---
+++ Pweb_FinalProjects-main/index.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<head>
+
+	<title>Library Management System </title>
+
+	<!-- Meta-Tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="keywords" content="Library Member Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+		<script type="application/x-javascript"> addEventListener("load",function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- //Meta-Tags -->
+
+	<!-- Style --> <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- Fonts -->
+		<link href="//fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+	<!-- //Fonts -->
+
+</head>
+<!-- //Head -->
+
+<!-- Body -->
+<body>
+
+	<h1 class="my-5 text-center">LIBRARY MANAGEMENT SYSTEM</h1>
+
+	<div class="container">
+		
+		<div>
+			<h2 class="text-center mb-3">Group</h2>
+			<p class="text-white text-center">* Venia Sollery Aliyya Hasna - 5025201161</p>
+			<p class="text-white text-center">* Bagus Maulana - 5025201162</p>
+			<p class="text-white text-center">* Erlangga Wahyu Utomo - 5025201118</p>
+			<p class="text-white text-center">* Christian Jeremia Halomoan Pakpahan - 5025201153</p>
+		</div>
+		
+        <div class="row mt-5">
+            <div class="col">
+                <div class="d-flex align-items-center">
+                    <a class="btn btn-primary btn-md text-center flex-fill " href="login.php" role="button"> LOGIN </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class=" d-flex align-items-center">
+                    <a class="btn btn-light btn-md text-center flex-fill " href="register.php" role="button"> REGISTER </a>
+                </div>    
+            </div>
+        </div>
+
+	</div>
+
+	<div class="footer w3layouts agileits">
+		<p> &copy; 2022 Library Member Login. All Rights Reserved </a></p>
+		
+	</div>
+
+</body>
+<!-- //Body -->
+
+</html>
---
+++ Pweb_FinalProjects-main/login.php
@@ -0,0 +1,72 @@
+<?php
+require('dbconn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row justify-content-center mb-3">
+            <div class="col-md-6">
+                <div class="my-4">
+                    <h1>Login</h1>
+                </div>
+                <form action="login.php" method="post">
+                    <div class="form-group mb-4">
+                        <label for="rollNumber">Roll Number</label>
+                        <input name="RollNo"type="text" class="form-control" id="rollNumber" placeholder="Roll Number">
+                        <small>Do not have an account yet? <a href="register.php">Register</a></small>
+                    </div>
+                    <div class="form-group mb-4">
+                        <label for="Password">Password</label>
+                        <input name="Password" type="password" class="form-control" id="Password" placeholder="Password">
+                    </div>
+                    <button type="submit" name="signin" value="Sign In" class="btn btn-dark">Login</button>
+                </form>
+            </div>
+         </div>
+    </div>
+
+    <?php
+        if(isset($_POST['signin']))
+        {$u=$_POST['RollNo'];
+         $p=$_POST['Password'];
+         
+
+         $sql="select * from PwebFP.user where RollNo='$u'";
+        
+         $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $x=$row['Password'];
+        $y=$row['Type'];
+        if(strcasecmp($x,$p)==0 && !empty($u) && !empty($p))
+          {echo "Login Successful";
+           $_SESSION['RollNo']=$u;
+           
+        
+          if($y=='Admin')
+           header('location:admin/index.php');
+          else
+              header('location:student/index.php');
+                
+          }
+        else 
+         { echo "<script type='text/javascript'>alert('Failed to Login! Incorrect RollNo or Password')</script>";
+        }
+    }
+    ?>
+
+
+
+
+</body>
+</html>
