b4f41bf97d4afe2c162a4296a3773e7e42f2286c
Venia <vsollery@gmail.com>
2022-12-08 11:04:18 +0700

    fix UI message

--- admin/message.php
+++ admin/message.php
@@ -2,12 +2,12 @@
 require('dbconn.php');
 ?>
 
-<?php 
+<?php
 if ($_SESSION['RollNo']) {
-    ?>
+?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -16,169 +16,133 @@ if ($_SESSION['RollNo']) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     </head>
+
     <body>
-         <!-- TOP NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="images/LMS.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            Library System
-        </a>
-        <ul class="navbar-nav ml-auto mr-5">
-            <li class="nav-item">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="images/user.png" width="30" height="30" class="nav-avatar" />
-                        <b class="caret"></b>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <a href="index.php">
-                            <button class="dropdown-item" type="button">Your Profile</button>
-                        </a>
-                        <a href="logout.php">
-                            <button class="dropdown-item" type="button">Logout</button>
-                        </a>
+        <!-- TOP NAVBAR -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="images/LMS.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                Library System
+            </a>
+            <ul class="navbar-nav ml-auto mr-5">
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="images/user.png" width="30" height="30" class="nav-avatar" />
+                            <b class="caret"></b>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <a href="index.php">
+                                <button class="dropdown-item" type="button">Your Profile</button>
+                            </a>
+                            <a href="logout.php">
+                                <button class="dropdown-item" type="button">Logout</button>
+                            </a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- side navbar -->
+        <div class="container-fluid">
+            <div class="row flex-nowrap">
+                <div class="col-auto col-md-3 col-xl-2 px-2 px-0 bg-dark">
+                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
+
+                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 mt-5 align-items-center align-items-sm-start" id="menu">
+                            <li class="nav-item ">
+                                <a href="index.php" class="nav-link align-middle px-0">
+                                    <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="message.php" class="nav-link align-middle px-0">
+                                    <i class="fs-4 bi-chat-dots-fill"></i> <span class="ms-2 d-none d-sm-inline">Messages</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="student.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-person-workspace"></i> <span class="ms-1 d-none d-sm-inline">Manage Students</span></a>
+                            </li>
+                            <li>
+                                <a href="book.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-book-half"></i> <span class="ms-1 d-none d-sm-inline">All Books</span></a>
+                            </li>
+                            <li>
+                                <a href="addbook.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-bookshelf"></i> <span class="ms-1 d-none d-sm-inline">Add Books</span></a>
+                            </li>
+                            <li>
+                                <a href="requests.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-archive-fill"></i> <span class="ms-1 d-none d-sm-inline">Issue/Return Requests </span> </a>
+                            </li>
+                            <li>
+                                <a href="recommendations.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Book Recommendations </span> </a>
+                            </li>
+                            <li>
+                                <a href="current.php" class="nav-link px-0 align-middle">
+                                    <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline"> Currently Issued Books </span> </a>
+                            </li>
+                            <li>
+                                <a href="logout.php">
+                                    <button type="button" class="btn btn-danger mt-3" style="width: 150px;"> <a href="logout.php">Logout</a></button>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </li>
-        </ul>    
-    </nav>
 
-    <!-- side navbar -->
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-2 px-0 bg-dark">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
-                    
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 mt-5 align-items-center align-items-sm-start" id="menu">
-                        <li class="nav-item ">
-                            <a href="index.php" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="message.php" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi-chat-dots-fill"></i> <span class="ms-2 d-none d-sm-inline">Messages</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="student.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-person-workspace"></i> <span class="ms-1 d-none d-sm-inline">Manage Students</span></a>
-                        </li>
-                        <li>
-                            <a href="book.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-book-half"></i> <span class="ms-1 d-none d-sm-inline">All Books</span></a>
-                        </li>
-                        <li>
-                            <a href="addbook.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-bookshelf"></i> <span class="ms-1 d-none d-sm-inline">Add Books</span></a>
-                        </li>
-                        <li>
-                            <a href="requests.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-archive-fill"></i> <span class="ms-1 d-none d-sm-inline">Issue/Return Requests </span> </a>
-                        </li>
-                        <li>
-                            <a href="recommendations.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Book Recommendations </span> </a>
-                        </li>
-                        <li>
-                            <a href="current.php" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline"> Currently Issued Books </span> </a>
-                        </li>
-                        <li>
-                            <a href="logout.php">
-                                <button type="button" class="btn btn-danger mt-3" style="width: 150px;"> <a href="logout.php">Logout</a></button>
-                            </a>
-                        </li>
-                    </ul>                   
-                </div>
-            </div>
-            
-
-                    <div class="span9">
-                    <div class="content">
-
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>Send a message</h3>
+                <!-- CONTENT -->
+                <div class="col mt-5">
+                    <div class=container-fluid>
+                        <h1 class="text-center">Send Message</h1>
+                    </div>
+                    <div class="container bg-light p-5 mt-5">
+                        <form action="message.php" method="post">
+                            <div class="form-group">
+                                <label for="RollNo">Receiver Roll Number:</label>
+                                <input type="text" name="RollNo" class="form-control" id="RollNO" placeholder="Roll Number" required>
                             </div>
-                            <div class="module-body">
-
-                                    <br >
-
-                                    <form class="form-horizontal row-fluid" action="message.php" method="post">
-                                        <div class="control-group">
-                                            <label class="control-label" for="Rollno"><b>Receiver Roll No:</b></label>
-                                            <div class="controls">
-                                                <input type="text" id="RollNo" name="RollNo" placeholder="RollNo" class="span8" required>
-                                            </div>
-                                        </div>
-                                        <div class="control-group">
-                                            <label class="control-label" for="Message"><b>Message:</b></label>
-                                            <div class="controls">
-                                                <input type="text" id="Message" name="Message" placeholder="Enter Message" class="span8" required>
-                                            </div>
-                                            <hr>
-                                        <div class="control-group">
-                                            <div class="controls">
-                                                <button type="submit" name="submit"class="btn">Add Message</button>
-                                            </div>
-                                        </div>
-                                    </form>
+                            <div class="form-group">
+                                <label for="Message">Message: </label>
+                                <input type="text" name="Message" class="form-control" id="Message" placeholder="Enter Message" required>
                             </div>
-                        </div>
+                            <button type="submit" name="submit" class="btn btn-primary center">Send Message</button>
+                        </form>
+                    </div>
 
-                        
-                        
-                    </div><!--/.content-->
                 </div>
-                    <!--/.span9-->
-                </div>
-            </div>
-            <!--/.container-->
+                <!-- END CONTENT -->
+            </div>            
         </div>
-<div class="footer">
-            <div class="container">
-                <b class="copyright">&copy; 2022 Library Management System </b>All rights reserved.
-            </div>
-        </div>
-        
-        <!--/.wrapper-->
+
+       
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
-        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
-        <script src="scripts/common.js" type="text/javascript"></script>
 
-<?php
-if(isset($_POST['submit']))
-{
-    $rollno=$_POST['RollNo'];
-    $message=$_POST['Message'];
+        <?php
+        if (isset($_POST['submit'])) {
+            $rollno = $_POST['RollNo'];
+            $message = $_POST['Message'];
 
-$sql1="insert into pwebfp.message (RollNo,Msg,Date,Time) values ('$rollno','$message',curdate(),curtime())";
+            $sql1 = "insert into pwebfp.message (RollNo, Msg, Date, Time) values ('$rollno','$message',curdate(),curtime())";
 
-if($conn->query($sql1) === TRUE){
-echo "<script type='text/javascript'>alert('Success')</script>";
-}
-else
-{//echo $conn->error;
-echo "<script type='text/javascript'>alert('Error')</script>";
-}
-    
-}
-?>
+            if ($conn->query($sql1) === TRUE) {
+                echo "<script type='text/javascript'>alert('Success')</script>";
+            } else { //echo $conn->error;
+                echo "<script type='text/javascript'>alert('Error')</script>";
+            }
+        }
+        ?>
     </body>
 
-</html>
+    </html>
 
 
-<?php }
-else {
+<?php } else {
     echo "<script type='text/javascript'>alert('Access Denied!!!')</script>";
 } ?>
