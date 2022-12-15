562033e2cea9008352ab0da5e35061e566add75f
Venia <vsollery@gmail.com>
2022-12-15 13:04:27 +0700

    fixed UI request

--- admin/requests.php
+++ admin/requests.php
@@ -2,11 +2,11 @@
 require('dbconn.php');
 ?>
 
-<?php 
+<?php
 if ($_SESSION['RollNo']) {
-    ?>
+?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="en">
 
     <head>
@@ -93,53 +93,51 @@ if ($_SESSION['RollNo']) {
                         </ul>
                     </div>
                 </div>
-                        <!--/.sidebar-->
+
+                <!-- CONTENT -->
+                <div class="col mt-5">
+                    <div class="container-fluid">
+                        <h1 class="text-center">Add Book</h1>
                     </div>
-                    <br>
-                    <br>
-                    <br>
-                    <div class="span3">
-                        <center>
-                            <a href="issue_requests.php" class="btn btn-info" ><image width="100px" src="images/book2.png"></image><p>Issue Requests</p></a>
-                        </center>
+                    <div class="container bg-light p-5 mt-5">
+                        <div class="row">
+
+                            <div class="col-md-4 my-3">
+                                <center>
+                                    <a href="issue_requests.php" class="btn btn-info">
+                                        <image class="rounded float-left" width="100px" src="images/book2.png"></image>
+                                        <p>Issue Requests</p>
+                                    </a>
+                                </center>
+                            </div>
+                            <div class="col-md-4 my-3">
+                                <center>
+                                    <a href="renew_requests.php" class="btn btn-info">
+                                        <image class="rounded mx-auto d-block" width="100px" src="images/book3.png"></image>
+                                        <p>Renew Request</p>
+                                    </a>
+                                </center>
+                            </div>
+                            <div class="col-md-4 my-3">
+                                <center>
+                                    <a href="return_requests.php" class="btn btn-info">
+                                        <image class="rounded float-right" width="100px" src="images/book4.png"></image>
+                                        <p>Return Requests</p>
+                                    </a>
+                                </center>
+                            </div>
+                        </div>
                     </div>
-                    <div class="span3">
-                        <center>
-                            <a href="renew_requests.php" class="btn btn-info"><image width="100px" src="images/book3.png"></image><p>Renew Request</p></a>
-                        </center>
-                    </div> 
-                    <div class="span3">
-                        <center>
-                            <a href="return_requests.php" class="btn btn-info"><image width="100px" src="images/book4.png"></image><p>Return Requests</p></a>
-                        </center>
-                    </div>
-                    <!--/.span3-->
-                    <!--/.span9-->
                 </div>
-            </div>
-            <!--/.container-->
-        </div>
-<div class="footer">
-            <div class="container">
-                <b class="copyright">&copy; 2022 Library Management System </b>All rights reserved.
+                <!-- END CONTENT -->
             </div>
         </div>
-        
-        <!--/.wrapper-->
-        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
-        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
-        <script src="scripts/common.js" type="text/javascript"></script>
-      
+
     </body>
 
-</html>
+    </html>
 
 
-<?php }
-else {
+<?php } else {
     echo "<script type='text/javascript'>alert('Access Denied!!!')</script>";
 } ?>
